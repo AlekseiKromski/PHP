d673a4b9e33697e5c20b864ae995305249bdfd8d
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-05-24 15:19:20 +0300

    Добавил код ассоциотивных массивов

--- list.php
+++ list.php
@@ -1,4 +1,29 @@
-<?php
-	$array = array(1,2.5,'astring',false);//Первый элемент является нулевым
-	echo "$array[1]"; // 0 - индекс элемента
+<?php 
+
+	//Массивы
+
+	$array = array(1,2.5,'astring','false');//Первый элемент является нулевым
+	//echo "$array[1]"; // 0 - индекс элемента
+	$array[] = 'New Element'; //Добовляем новый элемент
+
+	for ($i = 0; $i <= count($array); $i++ ){ //count - подсчитывает количество элементов массива
+		echo $array[$i] . '<br>';
+	}
+
+	//ассоциативные массивы
+
+	$list = array('age' => 12,'name' => 'alex','schoolBoy' => True ); // Ставим ключи к переменным
+	$list['age'] = 13;
+
+	echo getAvarge(array ('age' => 12,'name' => 13,'schoolBoy' => 16) );
+
+	$summa = 0;
+	function getAvarge ($massiv){
+		foreach ($massiv as $key => $value){//Цикл только для ассоциативных массивов
+			$summa += $value;
+		}
+		return $summa / count($massiv);
+	}
+
+
 ?>
