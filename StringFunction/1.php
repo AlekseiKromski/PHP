3df3e0c3ad0fa72eeef6782f7fe11d80344a5f53
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-06-03 13:25:21 +0300

    Функции строк(2)

--- StringFunction/1.php
+++ StringFunction/1.php
@@ -23,7 +23,34 @@
 	echo $str;
 	echo '<br>';
 	echo md5($str);
-	*/
+	
 	$str = "hello\nworld\n";
 	echo nl2br($str);
-?>
+	
+	
+	$str = '[i]Привет[/i], мое имя [b]Гриша из Растова[/b]';
+	echo $str;
+	echo "<br>";
+	//$str = str_replace('[b]', '<b>', $str);
+	//$str = str_replace('[/b]', '</b>', $str);
+	$search = ['[i]','[/i]','[b]','[/b]'];
+	$replace = ['<i>','</i>','<b>','</b>'];
+	//$str = str_replace($search, $replace, $str); // с учетом регистра 
+	$str = str_ireplace($search, $replace, $str); // без учета регистра
+	echo $str;
+
+	$str = '<i>Привет</i>, мое имя <b>Гриша из Растова</b><script>alert("xss")</script>';	
+	echo strip_tags($str);
+	//echo $str;
+	*/
+	$str = 'hellow';
+	$str = 'Привет';
+	echo mb_strlen($str);
+
+
+
+
+
+
+
+
