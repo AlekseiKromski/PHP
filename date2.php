7a51f6de58e733c50c3f22cd8c33a727cb76c20e
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-06-04 11:53:33 +0300

    Функции времени(2)

---
+++ date2.php
@@ -0,0 +1,8 @@
+<?php
+/*
+$date = date_create('2016-05-20');
+echo date_format($date, 'Y-m-d H:i:s');
+echo "<br>";
+date_add($date, date_interval_create_from_date_string('2 days + 2 hours + 10 minutes'));
+echo date_format($date, 'Y-m-d H:i:s');
+*/
