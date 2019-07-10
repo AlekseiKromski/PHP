e684b2c52929b351b3c1f0870b2e13a115e6a57e
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-07-11 00:15:25 +0300

    Наследование. Часть 2

--- PHP OOP/OOP_new/8/classes/Product.php
+++ PHP OOP/OOP_new/8/classes/Product.php
@@ -3,33 +3,22 @@ class Product{
     public $name;
     public $price;
     
-    public $cpu;
-    public $num_pages;
-    
     public function __construct($name, $price, $cpu = null, $num_pages = null) {
         $this->name = $name;
         $this->price = $price;
-        $this->cpu = $cpu;
-        $this->num_pages = $num_pages;
     }
     
-    public function getCpu(){
-        return $this->cpu;
+    public function getName(){
+        return $this->name;
     }
     
-    public function getNum_pages(){
-        return $this->num_pages;
+    public function getPrice(){
+        return $this->price;
     }
     
-    public function getProduct($type = 'notebook'){
-        $out = "<hr><b>О товаре: </b><br>"
+    public function getProduct(){
+        return $out = "<hr><b>О товаре: </b><br>"
                 . "Нименование: $this->name <br>"
                 . "Цена: $this->price <br>";
-        if($type == 'notebook'){
-            $out .= "Процессор $this->cpu <br>";
-        }else{
-            $out .= "Количество страниц: $this->num_pages <br>";
-        }
-        return $out;
     }
 }
--- PHP OOP/OOP_new/8/index.php
+++ PHP OOP/OOP_new/8/index.php
@@ -2,17 +2,21 @@
 error_reporting(-1);
 
 require_once 'classes/Product.php';
+require_once 'classes/book.php';
+require_once 'classes/notebook.php';
 
 function debug($data){
     echo '<pre>';
     print_r($data);
     echo '</pre>';
 }
-$book = new Product('Witcher', 20,null,1000);
-$notebook = new Product('Acer',500,'Intel i5');
+$book = new book('Witcher', 20,1000);
+$notebook = new notebook('Acer',500,'i5-5000');
+$notebook2 = new notebook('Hp',600,'i7-4772K');
 
 debug($book);
 debug($notebook);
 
-echo $book->getProduct($type='book');
+echo $book->getProduct();
 echo $notebook->getProduct();
+echo $notebook2->getProduct();
