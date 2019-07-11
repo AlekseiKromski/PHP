3547570fcd890f618d94cdc48ad7d3f83e39c8fd
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-07-11 23:36:08 +0300

    Модификаторы доступа ( Старт )

---
+++ PHP OOP/OOP_new/9/classes/Product.php
@@ -0,0 +1,24 @@
+<?php
+class Product{
+    public $name;
+    public $price;
+    public $public;
+    public function __construct($name, $price, $cpu = null, $num_pages = null) {
+        $this->name = $name;
+        $this->price = $price;
+    }
+    
+    public function getName(){
+        return $this->name;
+    }
+    
+    public function getPrice(){
+        return $this->price;
+    }
+    
+    public function getProduct(){
+        return $out = "<hr><b>О товаре: </b><br>"
+                . "Нименование: $this->name <br>"
+                . "Цена: $this->price <br>";
+    }
+}
