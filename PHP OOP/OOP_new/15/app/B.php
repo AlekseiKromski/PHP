5bf7200fd0e53c88a0d3193efdbfc49100f430dd
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-07-15 14:12:18 +0300

    Позднее статическое связывание

---
+++ PHP OOP/OOP_new/15/app/B.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace app;
+
+/**
+ * Description of B
+ *
+ * @author ShinSais
+ */
+class B extends A{
+    const TEST = "Class b";
+    
+    
+    
+    
+    
+    
+}
