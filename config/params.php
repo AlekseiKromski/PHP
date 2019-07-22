54e87c492e5cb92ceadd812bd649ce5df1e0a188
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-07-22 23:34:04 +0300

    Класс Реестр 2.1

---
+++ config/params.php
@@ -0,0 +1,12 @@
+<?php
+/*
+ * Файл возвращает массив с записанными данными
+ * */
+return [
+  'admin_email' => 'admin@admin.ru',
+  'Shop_name' => 'Shop php.com',
+  'pagination' => 3,
+  'smtp_login' => '',
+  'smtp_password' => '',
+
+];
