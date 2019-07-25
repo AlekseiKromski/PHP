49547db8fb2b43b18da5ba9062d9c7e2005cd59c
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-07-25 12:20:48 +0300

    Класс маршрутизатор. Часть 2

--- config/routes.php
+++ config/routes.php
@@ -5,16 +5,9 @@ use php\Router;
 //
 
 // default routes
-Router::add('^admin$', ["controller => 'Main', 'action' => 'index','prefix' => 'admin'"]); // от начала и до конца строки
-Router::add('^admin/?(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$', ['prefix' => 'admin']); // от начала и до конца строки
-
-Router::add('^$', ["controller => 'Main', 'action' => 'index'"]); // от начала и до конца строки
-Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
-
-/*
- * Router::add('^admin$', ['controller' => 'Main', 'action' => 'index', 'prefix' => 'admin']);
+Router::add('^admin$', ['controller' => 'Main', 'action' => 'index', 'prefix' => 'admin']);
 Router::add('^admin/?(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$', ['prefix' => 'admin']);
 
 Router::add('^$', ['controller' => 'Main', 'action' => 'index']);
 Router::add('^(?P<controller>[a-z-]+)/?(?P<action>[a-z-]+)?$');
-*/
+
--- vendor/php/core/Router.php
+++ vendor/php/core/Router.php
@@ -30,14 +30,72 @@ class Router
     //И в зависимости от того, что вернет matchRoute, будет вызывать контроллер
     public static function dispatch($url){
         if(self::matchRoute($url)){
-            echo 'ok';
+            $controller = 'app\controllers\\' . self::$route['prefix'] . self::$route['controller'] . 'Controller';
+            //Проверка на существование класса
+            if(class_exists($controller)){
+                //Создаем объект и передаем в него параметр $route
+                $controllerObject = new $controller(self::$route);
+                //Пример: indexAction
+                $action = self::lowerCamelCase(self::$route['action']) . 'Action';
+                //Проверка на существование метода
+                if(method_exists($controllerObject, $action)){
+                    //Вызывает action у объекта $controllerObject
+                    $controllerObject->$action();
+                }else{
+                    throw new \Exception("Метод $controller::$action не найден", 404);
+                }
+            }else{
+                throw new \Exception("Контроллер $controller не найден", 404);
+            }
+
         }else{
-            echo 'no';
+            throw new \Exception("Странница не найдена", 404);
         }
     }
 
     //Принемает url адресс, будет искать соответствие в массиве маршрутов
     public static function matchRoute($url){
-        return true;
+        foreach (self::$routes as $pattern => $route){ //Сравнивает правило и текущий маршрут
+            if(preg_match("#{$pattern}#", $url, $matches)){
+                //Берет массив matches и проверяет его на то, чтобы ключь являлся строкой
+                foreach ($matches as $k => $v){
+                    if(is_string($k)) {
+                        $route[$k] = $v;
+                    }
+                }
+                //Action по умолчанию = index
+                if(empty($route['action'])){
+                    $route['action'] = 'index';
+                }
+                if(!isset($route['prefix'])){
+                    $route['prefix'] = '';
+                }else{
+                    $route['prefix'] .= '\\';
+                }
+                //Обрабатываем имя контроллера
+                $route['controller'] = self::upperCamelCase($route['controller']);
+                //Добавляем в глобальный $route, значение локального $route
+                self::$route = $route;
+                return true;
+            }
+        }
+        return false;
+    }
+
+    //CamelCase
+    protected static function upperCamelCase($name){
+        //Ищем дефиз и удаляем его
+        $name = str_replace('-',' ',$name);
+        //Делаем каждую первую букву большой
+        $name = ucwords($name);
+        //Ищем пробел и удаляем его
+        $name = str_replace(' ','',$name);
+        return $name;
+    }
+
+    //camelCase
+    protected static function lowerCamelCase($name){
+        return lcfirst(self::upperCamelCase($name));
+
     }
 }
