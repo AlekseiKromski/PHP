88422aa9593bb1e27628a661c5bcfab50bf99690
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-07-26 14:56:13 +0300

    Класс View. Часть 2

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -4,10 +4,12 @@
 namespace app\controllers;
 
 
+use php\App;
+
 class MainController extends AppController
 {
 
     public function indexAction(){
-        $this->setMeta('Default','heeelololo','kse sef lfse');
+        $this->setMeta(App::$app->getProperty('Shop_name'),'lelele','ererer rere ere');
     }
 }
--- app/views/layouts/default.php
+++ app/views/layouts/default.php
@@ -5,8 +5,9 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <meta name="description" content="">
-    <title><? echo $meta ?></title>
+    <?php
+        echo $this->getMeta();
+    ?>
 </head>
 <body>
     <h1>Шаблон Default</h1>
