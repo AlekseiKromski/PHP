477b7c1846b8d245a61104797da157f7018dcc82
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-08-02 19:56:39 +0300

    move to old_work branch

--- public/errors/dev.php
+++
@@ -1,5 +0,0 @@
-<h1>Произошла ошибка</h1>
-<p><b>Код ошибки: </b><? echo $error_id; ?></p>
-<p><b>Текст ошибки: </b><? echo $error_str; ?></p>
-<p><b>Файл ошибки: </b><? echo $error_file; ?></p>
-<p><b>Строка ошибки: </b><? echo $error_line; ?></p>
