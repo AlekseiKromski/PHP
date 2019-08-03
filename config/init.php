acb518f5b4250a60b114a49c395e9bb760b5d2d0
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-08-03 22:14:52 +0300

    Конфигурация приложения 2

---
+++ config/init.php
@@ -0,0 +1,23 @@
+<?php
+
+// Константа нужна для класса обработчика ошибок
+define('DEBUG' , 1);
+//Корневой путь
+define('ROOT' , dirname(__DIR__));
+//Путь к папке public
+define('WWW' , ROOT . '/public');
+//Путь к папке app
+define('APP' , ROOT . '/app');
+//Путь к папке core
+define('CORE' , ROOT . '/vendor/php/core');
+//Путь к папке libs
+define('LIBS' , ROOT . '/vendor/php/core/libs');
+//Путь к папке кеша
+define('CACHE' , ROOT . '/tmp/cache');
+//Путь к папке конфига
+define('CONF' , ROOT . '/config');
+//Хранение расоположения шаблона сайта по умолчанию
+define('LAYOUT' , 'default');
+//Путь к папке кеша
+define('CACHE' , ROOT . '/tmp/cache');
+
