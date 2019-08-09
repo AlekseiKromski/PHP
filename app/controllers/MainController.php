5b02aaaca4c937680da865db684876ba825ad501
AlekseiKromski <aleksei.kromski@ivkhk.ee>
2019-08-09 13:01:12 +0300

    Вывод брендов

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -10,8 +10,9 @@ class MainController extends AppController
     //public $layout = 'test'; - Подключение другуго шаблона
 
     public function indexAction(){
+        $brands = \R::find('brand',"LIMIT 3");
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
-
+        $this->set(compact('brands'));
     }
 
 }
