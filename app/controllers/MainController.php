faa97862cd4ac2233ac6f709adc429916952cc82
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-08-22 10:06:00 +0300

    Вывод брендов

--- app/controllers/MainController.php
+++ app/controllers/MainController.php
@@ -7,7 +7,10 @@ use ishop\Cache;
 class MainController extends AppController {
 
     public function indexAction(){
+        $brands = \R::find('brand', 'LIMIT 3');
+        debug($brands);
         $this->setMeta('Главная страница', 'Описание...', 'Ключевики...');
+        $this->set(compact('brands'));
     }
 
 }
