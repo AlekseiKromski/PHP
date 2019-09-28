c3f891d17f6885914ad586a4d6f4b7ee9ac47486
aleksei.kromski <aleksei.kromski@ivkhk.ee>
2019-09-28 13:52:24 +0300

    Виджет выбора валют. Часть 1

--- config/config_db.php
+++ config/config_db.php
@@ -1,7 +1,7 @@
 <?php
 
 return [
-    'dsn' => 'mysql:host=192.168.1.13;dbname=php;charset=utf8',
+    'dsn' => 'mysql:host=localhost;dbname=php;charset=utf8',
     'user' => 'root',
     'pass' => '',
 ];
